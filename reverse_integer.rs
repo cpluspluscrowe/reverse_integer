31e4ef161073678391bdb7c8bcc22e37b635f7ac
Chad Crowe <ccrowe@ccrowe-mn1.linkedin.biz>
2020-03-27 19:31:40 -0700

    added is_palindrome function

--- reverse_integer.rs
+++ reverse_integer.rs
@@ -1,12 +1,19 @@
-
-
 fn main(){
-    let reversed = reverse(123);
-    println!("{}", reversed);
-    println!("Done!");
+    let number = 353;
+    let answer = is_palindrome(number);
+    println!("Is {0} a palindrome? Answer: {1}", number, answer);
+
+    let number = 355;
+    let answer = is_palindrome(number);
+    println!("Is {0} a palindrome? Answer: {1}", number, answer);    
 }
 
 pub fn reverse(x: i32) -> i32 {
     let building = x.to_string().chars().rev().collect::<String>();
     return building.parse::<i32>().unwrap();
+}
+
+pub fn is_palindrome(x: i32) -> bool {
+    let reversed = reverse(x);
+    return x == reversed;
 }
